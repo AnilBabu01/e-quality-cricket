e86cc86ec7416a0c6a9e5501a4fc41903d38b478
VivekBcloud <20vivektoppo@gmail.com>
2022-11-28 15:42:03 +0530

    rebase

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,16 +1,23 @@
-import { useState } from 'react'
-
-import Register from './screens/Auth/Register'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import CreatePassword from "./screens/Auth/createPassword/CreatePassword";
+import EmailLogin from "./screens/Auth/EmailLogin/EmailLogin";
+import Forgot from "./screens/Auth/Forgot/Forgot";
+import PhoneLogin from "./screens/Auth/Phonelogin/PhoneLogin";
+import Register from "./screens/Auth/Register";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <div className="App">
-      <Register/>
-        
-    </div>
-  )
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<EmailLogin />} />
+          <Route path="/phone" element={<PhoneLogin />} />
+          <Route path="/forget" element={<Forgot />} />
+          <Route path="/create" element={<CreatePassword />} />
+        </Routes>
+      </div>
+    </Router>
+  );
 }
 
-export default App
+export default App;
